5efeb061b766902a8714928672f80cf4b41c4c95
Jon Cram <jon@webignition.net>
2012-08-16 15:45:02 +0100

    Added valid content types

---
+++ src/webignition/WebResource/JsonDocument/JsonDocument.php
@@ -0,0 +1,22 @@
+<?php
+namespace webignition\WebResource\JsonDocument;
+
+use webignition\WebResource\WebResource;
+use webignition\InternetMediaType\Parser\Parser as InternetMediaTypeParser;
+
+/**
+ * 
+ */
+class JsonDocument extends WebResource
+{    
+    public function __construct() {
+        $validContentTypes = array(
+            'application/json'
+        );
+        
+        foreach ($validContentTypes as $validContentTypeString) {
+            $mediaTypeParser = new InternetMediaTypeParser();
+            $this->addValidContentType($mediaTypeParser->parse($validContentTypeString));
+        }
+    }      
+}
